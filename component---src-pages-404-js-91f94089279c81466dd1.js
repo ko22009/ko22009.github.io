a5a508448a66c0794f3b34865638935d90a16daa
ko2 <ko22009@yandex.ru>
2022-06-22 23:43:52 +0500

    Updates

---
+++ component---src-pages-404-js-91f94089279c81466dd1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_gatsby_site=self.webpackChunkmy_gatsby_site||[]).push([[883],{429:function(e,t,n){n.r(t);var a=n(7294),l=n(8678),o=n(262);t.default=function(e){var t=e.location;return a.createElement(l.Z,{location:t},a.createElement(o.Z,{title:"404: Not Found"}),a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"Page not found. ",a.createElement("a",{href:"/"},"Go to main page")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-91f94089279c81466dd1.js.map
