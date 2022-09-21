36f239e4e925102c7c9a2f767755acaf8b8f4e70
ko2 <ko22009@yandex.ru>
2022-09-21 21:00:26 +0500

    add new project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -33,13 +33,21 @@ const Contacts = ({ location }) => {
     <Layout className="layout" location={location} Footer={Footer}>
       <Seo title={siteTitle} />
       <h1>{siteTitle}</h1>
-      <h2>Burger</h2>
       <div style={{ maxWidth: "800px" }}>
+        <h2>Burger</h2>
         <iframe
           style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
           width="800"
           height="450"
           src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQDPj0Vqqd2RIGHwwXsGEdQ%2F%25D0%25B1%25D1%2583%25D1%2580%25D0%25B3%25D0%25B5%25D1%2580%25D0%25BD%25D0%25B0%25D1%258F%3Fnode-id%3D0%253A1"
+          allowFullScreen
+        ></iframe>
+        <h2>Volunteer</h2>
+        <iframe
+          style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
+          width="800"
+          height="450"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FIyIKzcvd6274cu4NRCLPvB%2Fvolunteer%3Fnode-id%3D0%253A1"
           allowFullScreen
         ></iframe>
         <h2>Pizza</h2>
